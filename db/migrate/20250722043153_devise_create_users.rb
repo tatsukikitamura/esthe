dbb742c64d0c1ce50357b37d0d4a446549f27e42
tatsuki <tatsuki1114@fuji.waseda.jp>
2025-09-12 12:35:00 +0900

    Remove redundant `id` column from Devise user migration and add a new line in Gemfile.

--- db/migrate/20250722043153_devise_create_users.rb
+++ db/migrate/20250722043153_devise_create_users.rb
@@ -13,7 +13,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-      t.integer :id
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
